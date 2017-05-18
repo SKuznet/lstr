30e0a4fc7a8e3e37fc10f06a59a7fb4de2ae856a
Sergey Kuznetsov <SKuznetsov@idbs.com>
2017-05-18 18:10:01 +0300

    LSTR-2811 add Hello class with message

--- untitled/src/Hello.java
+++ untitled/src/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        System.out.println("Barsik");
+        System.out.println("Rigik");
+        System.out.println("Makar");
     }
 }
